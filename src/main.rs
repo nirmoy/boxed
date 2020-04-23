fbae983c923e2dac8cd29ac7cd50ad64f5ff0a52
Nirmoy Das <nirmoy.das@amd.com>
2020-04-23 15:34:55 +0200

    use Iterators to find max string

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,12 @@ use std::io;
 use std::io::prelude::*;
 
 fn boxed(lines: Vec<String>) {
-    let mut max_line = 0;
     let mut first_line = String::new();
     let mut last_line = String::new();
 
-    for line in &lines {
-        if max_line < line.len() {
-            max_line = line.len();
-        }
-    }
+    let max_line = lines.iter().fold(0, |acc, item| {
+                    if item.len() > acc { item.len() } else { acc }
+    });
 
     first_line.push('╔');
     last_line.push('╚');
